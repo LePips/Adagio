c052346a047f9c5dd3ef54188e16c264ad6a4924
Ethan Pippin <ethanpippin2343@gmail.com>
2020-01-14 14:34:43 -0700

    Initial Commit

---
+++ Adagio/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Adagio
+//
+//  Created by Ethan Pippin on 1/14/20.
+//  Copyright © 2020 Ethan Pippin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
